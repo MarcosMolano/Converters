353cbe068dbd4801879a30975401c969b90c78b6
Marcos Molano Soriano <Marcos.Molano@tajamar365.com>
2019-05-06 12:47:43 +0200

    first commit

---
+++ Converters/Converters/Converted/Descuento.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Xamarin.Forms;
+
+namespace Converters.Converted
+{
+    public class Descuento : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value != null)
+            {
+                if (value.ToString() == "")
+                {
+                    return 0;
+                }
+                else
+                {
+                    int precio = int.Parse(value.ToString());
+                    float preciodescuento = precio * 50 / 100;
+                    return preciodescuento;
+                }
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
